52da1574cfaa1d54c569037e471ee2031bd4fd86
Ranxin Li <ranxinli2024@gmail.com>
2025-06-22 00:16:37 -0700

    upload 0621 part of transfer learning

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from tensor_demo import tensor_demo
 from nn_training import redirect_nn_training
+from transfer_learning import transfer_learning_redirect_output
 # import torch, torchvision
 def main():
-    tensor_demo()
-    redirect_nn_training()
+    # tensor_demo()
+    # redirect_nn_training()
     # print(torch.__version__)
     # print(torchvision.__version__)
+    transfer_learning_redirect_output()
     
 if __name__=="__main__":
     main()
